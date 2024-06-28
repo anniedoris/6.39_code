a2fccbe6c32eb19ad90d448ac33b3a20a5cb2e5d
anniedoris <anniecd88@gmail.com>
2024-06-28 17:43:26 -0400

    week 3 lab done

---
+++ week3/lab/lab.py
@@ -0,0 +1,32 @@
+## Question 1b
+# Clyinders - standardize
+# Displacement - standardize
+# Horsepower - standardize
+# Weight - standardize
+# Acceleration - standardize
+# Model year - drop
+# origin - one hot
+# car name - leave out
+
+## Question 1c
+# Create classes and one hot encode, or bag of words
+
+## Question 1d
+# probably either method 3 or 4 would be ok
+
+## Question 2c
+# the order of the sentences is completely lost, which is an issue
+
+## Question 2d
+# You could make a list of article words which you then scrape to eliminiate
+
+## Question 3c
+# Not very well - you have lost the adjacency of pixels if you flatten them into 1D
+
+## Quesiton 3d
+# You could encode adjacency of pixels as features, or total number of black versus white pixels maybe
+# or specific distinguishing features
+
+## Question 4
+# Question 4a - probably not very well, given that this is seems to be a lot of features
+# Question 4b - I would maybe use a fourier transform to provide amplitudes of the most dominant frequencies as features
